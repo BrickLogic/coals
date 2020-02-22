1519f461b0038129d1947277df2dae8e6f914a0b
rastopyr <rastopyr@gmail.com>
2020-02-22 21:00:34 +0200

    patch(infrastructure): add infra for tests and lint

--- coals.spec.ts
+++ coals.spec.ts
@@ -13,35 +13,70 @@ describe("Coals", () => {
         expect(s.completed).toBe(false);
     });
 
-    it("it should add subscribers on subscribe", () => {
+    it("should add subscribers on subscribe", () => {
         const s = coals(123);
 
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
         s.subscribe(() => {});
         expect(s.subscribers.length).toBe(1);
     });
 
+    it("should call subscribe callback on new value", () => {
+        return new Promise(resolve => {
+            const s = coals(123);
+            const submittedValue = 321;
 
-    it("it should call subscribe callback on new value", (done) => {
-        const s = coals(123);
-        const submittedValue = 321;
+            s.subscribe(nextValue => {
+                expect(nextValue).toBe(submittedValue);
+                resolve();
+            });
 
-        s.subscribe((nextValue) => {
-            expect(nextValue).toBe(submittedValue);
-            done();
+            s.next(submittedValue);
         });
-
-        s.next(submittedValue);
     });
 
-    it("it should call complete callback when stream is completed", (done) => {
-        const s = coals(123);
+    it("should call complete callback when stream is completed", () => {
+        return new Promise(resolve => {
+            const s = coals(123);
 
-        s.subscribe(() => {}, () => {
-            expect(s.completed).toBe(true);
+            s.subscribe(
+                // eslint-disable-next-line @typescript-eslint/no-empty-function
+                () => {},
+                () => {
+                    expect(s.completed).toBe(true);
 
-            done();
+                    resolve();
+                }
+            );
+
+            s.complete();
         });
+    });
 
-        s.complete();
+    it("should set value to stream", () => {
+        const s = coals(123);
+        const newValue = 321;
+
+        s.next(newValue);
+        expect(s.value).toBe(newValue);
+    });
+
+    it("should notify subscribers of changes", () => {
+        return new Promise(resolve => {
+            const s = coals(123);
+            const newValue = 321;
+
+            s.subscribe(
+                nextValue => {
+                    expect(nextValue).toBe(newValue);
+                    resolve();
+                },
+                // eslint-disable-next-line @typescript-eslint/no-empty-function
+                () => {}
+            );
+
+            s.next(newValue);
+            s.complete();
+        });
     });
 });
--- coals.ts
+++ coals.ts
@@ -1,24 +1,23 @@
-
-type NextSubscriber<T> = (nextValue:  T) => void;
+type NextSubscriber<T> = (nextValue: T) => void;
 type CompleteSubscribe = () => void;
 type ErrorSubscribe = <T extends Error>(err: T) => void;
 
 class Subscriber<T> {
     public subscribed: boolean;
 
-    constructor(
-        readonly nextSubscriber?: NextSubscriber<T>,
-        readonly completeSubscriber?: CompleteSubscribe,
-        readonly errorSubscriber?: ErrorSubscribe
+    public constructor(
+        private readonly nextSubscriber?: NextSubscriber<T>,
+        private readonly completeSubscriber?: CompleteSubscribe,
+        private readonly errorSubscriber?: ErrorSubscribe
     ) {
         this.subscribed = true;
     }
 
-    public unsubscribe() {
+    public unsubscribe(): void {
         this.subscribed = false;
     }
 
-    public complete() {
+    public complete(): void {
         this.unsubscribe();
 
         if (this.completeSubscriber) {
@@ -26,41 +25,58 @@ class Subscriber<T> {
         }
     }
 
-    public next(value: T) {
+    public next(value: T): void {
         if (this.subscribed && this.nextSubscriber) {
-            this.nextSubscriber(value);
+            try {
+                this.nextSubscriber(value);
+            } catch (e) {
+                if (this.errorSubscriber) {
+                    this.errorSubscriber(e);
+                }
+            }
         }
-    };
+    }
 }
 
 class Stream<T> {
-    constructor(public value: T, public completed: boolean, public subscribers: readonly Subscriber<T>[]) {}
+    public constructor(
+        public value: T,
+        public completed: boolean,
+        public subscribers: readonly Subscriber<T>[]
+    ) {}
 
-    public next(nextValue: T) {
+    public next(nextValue: T): void {
         if (this.completed) {
             throw new Error();
         }
 
         this.value = nextValue;
 
-        this.subscribers.forEach((s) => s.next(this.value))
+        this.subscribers.forEach(s => s.next(this.value));
     }
 
-    public subscribe(nextCallback?: NextSubscriber<T>, completeCallback?: CompleteSubscribe, errorCallback?: ErrorSubscribe) {
-        this.subscribers = [...this.subscribers, new Subscriber<T>(nextCallback, completeCallback, errorCallback)];
+    public subscribe(
+        nextCallback?: NextSubscriber<T>,
+        completeCallback?: CompleteSubscribe,
+        errorCallback?: ErrorSubscribe
+    ): void {
+        this.subscribers = [
+            ...this.subscribers,
+            new Subscriber<T>(nextCallback, completeCallback, errorCallback)
+        ];
     }
 
-    public complete() {
+    public complete(): void {
         this.completed = true;
 
-        this.subscribers.forEach((subscriber) => {
+        this.subscribers.forEach(subscriber => {
             subscriber.complete();
             subscriber.unsubscribe();
-        })
+        });
     }
 }
 
-const coals = function <T>(initialValue: T) {
+const coals = <T>(initialValue: T): Stream<T> => {
     return new Stream(initialValue, false, []);
 };
 
