9592311a3733423bc5b0723705d0dd5cdc04cb3d
rastopyr <rastopyr@gmail.com>
2020-02-23 12:46:25 +0200

    patch: cleanup and add on logic

--- coals.ts
+++ coals.ts
@@ -1,8 +1,51 @@
-type StreamType<T> = T | undefined;
 
-type NextSubscriber<T> = (nextValue: StreamType<T>) => void;
-type CompleteSubscribe = () => void;
-type ErrorSubscribe = <T extends Error>(err: T) => void;
+/*
+ * const v = coals(1);
+ * const z = coals(2);
+ *
+ *
+ * const x = combine([v, z], (v, z) => {
+ *   return v + z;
+ * });
+ *
+ * console.log(x) // 3
+ *
+ *
+ * const y = merge(
+ *   v,
+ *   z
+ * );
+ *
+ * console.log(y); // 2
+ * v(8);
+ *
+ * console.log(y()); // 8
+ *
+ * const iv = interval();
+ *
+ * const i = interval(10);
+ *
+ * const unsubscribe = i.subscrine(() => {
+ *   console.log('count each 10 ms')
+ * });
+ *
+ * const unsub = on([v, z], (vv, vz) => {
+ *  console.log(vv + vz);
+ * })
+ *
+ * // will log 10
+ *
+ * v(20);
+ *
+ * // will log 22
+ *
+ * unsub();
+ *
+ * v(30);
+ *
+ * // will log nothing
+ *
+ * */
 
 type AtomResetCallback = () => void;
 type AtomChangeCallback<T> = (newState: T) => void;
@@ -155,232 +198,3 @@ export function combine<T, K>(
         return producer(...args);
     };
 }
-
-/*
- * const v = coals(1);
- * const z = coals(2);
- *
- *
- * const x = combine([v, z], (v, z) => {
- *   return v + z;
- * });
- *
- * console.log(x) // 3
- *
- *
- * const y = merge(
- *   v,
- *   z
- * );
- *
- * console.log(y); // 2
- * v(8);
- *
- * console.log(y); // 8
- *
- * const iv = interval();
- *
- * const i = interval(10);
- *
- * const unsubscribe = i.subscrine(() => {
- *   console.log('count each 10 ms')
- * });
- *
- * */
-
-class Subscription {
-    public subscribed = true;
-
-    public constructor(private onUnsubscribe: TeardownCallback | void) {}
-
-    public unsubscribe(): void {
-        if (!this.subscribed) {
-            return;
-        }
-
-        this.subscribed = false;
-
-        if (this.onUnsubscribe) {
-            this.onUnsubscribe();
-        }
-    }
-}
-
-export const SUBSCRIBER_SYMBOL = Symbol("SUBSCRIBER_SYMBOL");
-
-// TODO: extends interface with Subscription class ???
-export class Subscriber<T> {
-    public subscription?: Subscription;
-
-    public [SUBSCRIBER_SYMBOL]: true;
-
-    public constructor(
-        private readonly nextSubscriber?: NextSubscriber<T>,
-        private readonly completeSubscriber?: CompleteSubscribe,
-        private readonly errorSubscriber?: ErrorSubscribe
-    ) {}
-
-    public subscribe(onUnsubscribe: TeardownCallback | void): Subscription {
-        this.subscription = new Subscription(onUnsubscribe);
-
-        return this.subscription;
-    }
-
-    public unsubscribe(): void {
-        if (!this.subscription || !this.subscription.subscribed) {
-            throw new Error("Subscriber not subscribed yet");
-        }
-
-        this.subscription.unsubscribe();
-    }
-
-    public complete(): void {
-        this.unsubscribe();
-
-        if (this.completeSubscriber) {
-            this.completeSubscriber();
-        }
-    }
-
-    public next(value: StreamType<T>): void {
-        if (this.subscription && this.subscription.subscribed && this.nextSubscriber) {
-            try {
-                this.nextSubscriber(value);
-            } catch (e) {
-                if (this.errorSubscriber) {
-                    this.errorSubscriber(e);
-                }
-            }
-        }
-    }
-}
-
-type TeardownCallback = () => void;
-export type ObserverSubscription<T> = (observer: Subscriber<T>) => TeardownCallback | void;
-
-export const OBSERVABLE_SYMBOL = Symbol("OBSERVABLE_SYMBOL");
-
-export class Observable<T> {
-    public completed: boolean;
-
-    public observers: readonly Subscriber<T>[];
-
-    public source?: Observable<T>;
-
-    public [OBSERVABLE_SYMBOL]: true;
-
-    public constructor(private observerSubscription?: ObserverSubscription<T>) {
-        this.completed = false;
-        this.observers = [];
-    }
-
-    public subscribe(
-        nextCallback?: NextSubscriber<T> | Subscriber<T>,
-        completeCallback?: CompleteSubscribe,
-        errorCallback?: ErrorSubscribe
-    ): Subscription {
-        if (this.source) {
-            return this.source.subscribe(nextCallback, completeCallback, errorCallback);
-        }
-
-        let teardown: TeardownCallback | void;
-
-        const innerCompleteCallback = (): void => {
-            this.onComplete();
-            if (completeCallback) {
-                completeCallback();
-            }
-        };
-
-        const innerNextCallback: NextSubscriber<T> = nextValue => {
-            if (!nextCallback) {
-                return;
-            }
-
-            if (nextCallback instanceof Subscriber) {
-                nextCallback.next(nextValue);
-                return;
-            }
-
-            nextCallback(nextValue);
-        };
-
-        const subscriber = new Subscriber<T>(
-            innerNextCallback,
-            innerCompleteCallback,
-            errorCallback
-        );
-        const subscription = subscriber.subscribe(() => {
-            this.onUnsubscribe(subscriber);
-            if (teardown) {
-                teardown();
-            }
-        });
-
-        if (this.observerSubscription) {
-            teardown = this.observerSubscription(subscriber);
-            this.observers = [...this.observers, subscriber];
-        }
-
-        return subscription;
-    }
-
-    public onUnsubscribe(subscriber: Subscriber<T>): void {
-        this.observers = this.observers.filter(o => o !== subscriber);
-    }
-
-    private onComplete(): void {
-        this.completed = true;
-    }
-
-    public complete(): void {
-        this.completed = true;
-
-        this.observers.forEach(subscriber => {
-            subscriber.complete();
-            subscriber.unsubscribe();
-        });
-    }
-}
-
-export class Subject<T> extends Observable<T> {
-    public constructor() {
-        // eslint-disable-next-line constructor-super
-        super(() => undefined);
-    }
-
-    public next(nextValue: StreamType<T>): void {
-        if (this.completed) {
-            throw new Error("Observable already completed");
-        }
-
-        this.observers.forEach(o => o.next(nextValue));
-    }
-}
-
-export const interval = (intervalTime: number): Observable<number> => {
-    return new Observable(s => {
-        let counter = 0;
-        const i = setInterval(() => {
-            counter += 1;
-            s.next(counter * intervalTime);
-        }, intervalTime);
-
-        return () => {
-            clearInterval(i);
-        };
-    });
-};
-
-export const timeout = (timeoutTime: number): Observable<void> => {
-    return new Observable(s => {
-        const t = setTimeout(() => {
-            s.next();
-            s.complete();
-        }, timeoutTime);
-
-        return () => {
-            clearTimeout(t);
-        };
-    });
-};
